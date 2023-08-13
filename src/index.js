7c0eaaaa04f69e77f971f1bd738d32f741e19fbf
Alexei <0alexchebanu@gmail.com>
2023-08-13 22:42:09 +0300

    Add CRUD, API fetch

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import ReactDOM from "react-dom/client";
 // import App from "./App";
 
 import StarRating from "./StarRating";
+import App from "./App copy";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
+    <App />
   </React.StrictMode>
 );
